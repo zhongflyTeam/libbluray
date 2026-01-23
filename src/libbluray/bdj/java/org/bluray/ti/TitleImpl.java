64947ad6d42d399598aa4353d154e80b81e1c80b
Petri Hintukainen <phintuka@gmail.com>
2026-01-23 15:52:48 +0200

    Trim trailing space

--- src/libbluray/bdj/java/org/bluray/ti/TitleImpl.java
+++ src/libbluray/bdj/java/org/bluray/ti/TitleImpl.java
@@ -28,16 +28,16 @@ public class TitleImpl implements Title {
     public PlayList[] getPlayLists() {
         if (bdjo == null)
             return new PlayList[0];
-       
+
         org.videolan.bdjo.PlayListTable plt = bdjo.getAccessiblePlaylists();
-        
+
         // If accessToAll flag is set, enumerate all playlists on the disc
         if (plt.isAccessToAll()) {
             String[] files = Libbluray.listBdFiles("BDMV" + java.io.File.separator + "PLAYLIST", false);
             if (files == null) {
                 return new PlayList[0];
             }
-            
+
             java.util.Vector allPlaylists = new java.util.Vector();
             for (int i = 0; i < files.length; i++) {
                 String file = files[i];
@@ -50,7 +50,7 @@ public class TitleImpl implements Title {
             allPlaylists.copyInto(playlists);
             return playlists;
         }
-        
+
         String[] playlistNames = plt.getPlayLists();
         PlayList[] playlists = new PlayList[playlistNames.length];
         for (int i = 0; i < playlistNames.length; i++)
